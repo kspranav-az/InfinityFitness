5c9b56fc1f81435c386293036abe5af1bf7b9cf9
Harsh Raj A <121369929+1iamharshraj@users.noreply.github.com>
2024-09-05 12:43:43 +0530

    added a few thing

--- app/src/main/java/com/example/infinityfitness/register.kt
+++ app/src/main/java/com/example/infinityfitness/register.kt
@@ -1,6 +1,8 @@
 package com.example.infinityfitness
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,30 @@ class register : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val spinner: Spinner = findViewById(R.id.sex)
+// Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.SEX,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears.
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner.
+            spinner.adapter = adapter
+        }
+        val spinner1: Spinner = findViewById(R.id.mop)
+// Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.MODE_OF_PAYMENT,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears.
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner.
+            spinner1.adapter = adapter
         }
     }
 }

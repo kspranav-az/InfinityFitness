315c751316f4b41693c1f012a472b041753aaa5c
Harsh Raj A <121369929+1iamharshraj@users.noreply.github.com>
2024-09-08 19:24:17 +0530

    added few layouts and activities

---
+++ app/src/main/java/com/example/infinityfitness/CustData.kt
@@ -0,0 +1,20 @@
+package com.example.infinityfitness
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CustData : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.cust_data)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}

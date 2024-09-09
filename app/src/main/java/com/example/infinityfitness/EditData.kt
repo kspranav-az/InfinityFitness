6cdacf38f94224ee46fca3350d6582ff5bca64d9
Harsh Raj A <121369929+1iamharshraj@users.noreply.github.com>
2024-09-09 14:37:48 +0530

    Added two new layout and their frontend along with a few other changes

--- app/src/main/java/com/example/infinityfitness/EditData.kt
+++ app/src/main/java/com/example/infinityfitness/EditData.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class EditCustData : AppCompatActivity() {
+class EditData : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.edit_cust_data)
+        setContentView(R.layout.edit_data)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
